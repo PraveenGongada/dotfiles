4476be187339c6881f0dd2f5090bac63c834ac5b
Praveen kumar <praveenkumar@Praveens-MacBook-Pro.local>
2024-11-24 21:32:31 +0530

    :sparkles: updated maple font, tmux display, wezterm full screen open

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,4 +1,10 @@
 local wezterm = require("wezterm")
+local mux = wezterm.mux
+
+wezterm.on("gui-startup", function(cmd)
+	local _, _, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
 
 local config = {}
 
