73bcc2b50c5ad0da725c86a688f73fa891b163d3
PraveenGongada <praveengongada0603@gmail.com>
2024-11-27 23:39:37 +0530

    :dizzy: moved and removed nvim plugins

--- nvim/lua/plugins/defaults.lua
+++ nvim/lua/plugins/defaults.lua
@@ -7,4 +7,16 @@ return {
 		"mbbill/undotree",
 		cmd = "UndotreeToggle",
 	},
+	{	"stevearc/dressing.nvim",
+		event = "VeryLazy",
+		config = function()
+			require("dressing").setup({})
+		end,
+	},
+	{
+		"kylechui/nvim-surround",
+		event = { "BufReadPost", "BufNewFile" },
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		config = true,
+	}
 }
--- nvim/lua/plugins/surround.lua
+++
@@ -1,6 +0,0 @@
-return {
-	"kylechui/nvim-surround",
-	event = { "BufReadPost", "BufNewFile" },
-	version = "*", -- Use for stability; omit to use `main` branch for the latest features
-	config = true,
-}
